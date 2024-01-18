7f708240b4da861318ab4287fc5dbc995849fa9e
Thepizza-Xbox <156123920+raiz-pc@users.noreply.github.com>
2024-01-18 16:27:09 -0600

    Added random object obstacles, will expand map soon

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,25 @@ namespace myImages {
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 `;
+            case "image2":
+            case "forestMonkey8":return img`
+. . . . . f f f f f . . . . . . 
+. . . . f e e e e e f . . . . . 
+. . . f d d d d d d e f . . . . 
+. . f d f f d d f f d f f . . . 
+. c d d d e e d d d d e d f . . 
+. c d c d d d d c d d e f f . . 
+. c d d c c c c d d d e f f f f 
+. . c d d d d d d d e f f b d f 
+. . . c d d d d e e f f f d d f 
+. . . . f f f e e f e e e f f f 
+. . . . f e e e e e e e f f f . 
+. . . f e e e e e e f f f e f . 
+. . f f e e e e f f f f f e f . 
+. f b d f e e f b b f f f e f . 
+. f d d f f f f d d b f f f f . 
+. f f f f f f f f f f f f f . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,18 +1,3 @@
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    mySprite,
-    [img`
-        . . . . . . . . . . . . 
-        . . f f 1 1 1 f f . . . 
-        . . . 2 2 2 2 2 . . . . 
-        . c 2 2 2 2 2 2 2 c . . 
-        . c f f 2 2 2 f f c . . 
-        . c f f c c c f f c . . 
-        `],
-    100,
-    false
-    )
-})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
@@ -58,6 +43,45 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . 
+        . . f f 1 1 1 f f . . . 
+        . . . 2 2 2 2 2 . . . . 
+        . c 2 2 2 2 2 2 2 c . . 
+        . c f f 2 2 2 f f c . . 
+        . c f f c c c f f c . . 
+        `],
+    100,
+    false
+    )
+})
+function change_Level (Level_Number: number) {
+    if (Level_Number == 1) {
+        tiles.setCurrentTilemap(tilemap`starfin`)
+    } else if (Level_Number == 2) {
+        tiles.setCurrentTilemap(tilemap`level2`)
+    }
+}
+function PlaceObstacles (row: number, column: number) {
+    for (let index2 = 0; index2 <= column; index2++) {
+        for (let index = 0; index <= row; index++) {
+            if (Math.percentChance(5)) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(index2, index), assets.tile`FINISHLIEN`) || tiles.tileAtLocationEquals(tiles.getTileLocation(index2, index), assets.tile`myTile4`)) {
+                	
+                } else {
+                    if (Math.percentChance(50)) {
+                        tiles.setTileAt(tiles.getTileLocation(index2, index), sprites.builtin.forestTiles0)
+                    } else {
+                        tiles.setTileAt(tiles.getTileLocation(index2, index), assets.tile`obstacle-wall`)
+                    }
+                }
+            }
+        }
+    }
+}
 let mySprite: Sprite = null
 scene.setBackgroundImage(img`
     bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
@@ -193,6 +217,8 @@ mySprite = sprites.create(img`
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 let levels = [tileUtil.createSmallMap(tilemap`starfin`), 1]
+let CurrentLevel = 0
+PlaceObstacles(100, 100)
 forever(function () {
     let level1: tiles.TileMapData = null
     let tilemap2: tiles.TileMapData = null
