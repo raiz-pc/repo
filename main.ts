021fb0076e99a155b057a73a8a68691bc13c7f71
Thehut-Xbox <156123362+Thehut-Xbox@users.noreply.github.com>
2024-01-19 16:30:04 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, Gokublack.ts, car.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite,
     [img`
         . . . . . . . . . . . . 
-        . . . . 2 2 2 . . . . . 
+        . . f f f f f f f . . . 
         . . . 2 2 2 2 2 . . . . 
         . c 4 4 2 2 2 4 4 c . . 
         . c f f 1 1 1 f f c . . 
@@ -17,7 +17,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
     [img`
-        . . . . . . 1 1 1 . 2 2 
+        . . . . . . 1 1 1 . f f 
         . 2 2 2 2 2 2 2 2 2 2 . 
         2 2 f f 2 2 2 2 2 f f c 
         c c f f c c c c c f f c 
@@ -32,7 +32,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
     [img`
-        2 2 . 1 1 1 . . . . . . 
+        f f . 1 1 1 . . . . . . 
         . 2 2 2 2 2 2 2 2 2 2 . 
         c f f 2 2 2 2 2 f f 2 2 
         c f f c c c c c f f c c 
@@ -42,6 +42,51 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     100,
     false
     )
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTileperson`, function (sprite, location) {
+    for (let index = 0; index < 1; index++) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . 
+            . . . . . 4 4 . . . . . 
+            . . . . 4 5 5 4 . . . . 
+            . . . . 2 5 5 2 . . . . 
+            . . . . . 2 2 . . . . . 
+            . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . 
+            . . . . . . . . . . . . 
+            . . . . . . . . . . . . 
+            . . . . . . . . . . . . 
+            . . . . . . . . . . 4 . 
+            . . . . 2 . . . . 4 4 . 
+            `,img`
+            . 3 . . . . . . . . . . 
+            . 3 3 . . . . . . . . . 
+            . 3 d 3 . . 4 4 . . 4 4 
+            . . 3 5 3 4 5 5 4 4 d d 
+            . . 3 d 5 d 1 1 d 5 5 d 
+            . . 4 5 5 1 1 1 1 5 1 1 
+            `,img`
+            . . . . . . . . . . . . 
+            . . . . . . . . . . . . 
+            . . . . . b b . b b b . 
+            . . . . b 1 1 b 1 1 1 b 
+            . . b b 3 1 1 d d 1 d d 
+            . b 1 1 d d b b b b b 1 
+            `,img`
+            f f . 1 1 1 . . . . . . 
+            . 2 2 2 2 2 2 2 2 2 2 . 
+            c f f 2 2 2 2 2 f f 2 2 
+            c f f c c c c c f f c c 
+            c c c c c c c c c c c . 
+            . . . . . . . . . . . . 
+            `],
+        100,
+        false
+        )
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -67,15 +112,15 @@ function change_Level (Level_Number: number) {
 }
 function PlaceObstacles (row: number, column: number) {
     for (let index2 = 0; index2 <= column; index2++) {
-        for (let index = 0; index <= row; index++) {
+        for (let index3 = 0; index3 <= row; index3++) {
             if (Math.percentChance(5)) {
-                if (tiles.tileAtLocationEquals(tiles.getTileLocation(index2, index), assets.tile`FINISHLIEN`) || tiles.tileAtLocationEquals(tiles.getTileLocation(index2, index), assets.tile`myTile4`)) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(index2, index3), assets.tile`FINISHLIEN`) || tiles.tileAtLocationEquals(tiles.getTileLocation(index2, index3), assets.tile`myTile4`)) {
                 	
                 } else {
                     if (Math.percentChance(50)) {
-                        tiles.setTileAt(tiles.getTileLocation(index2, index), assets.tile`myTileperson`)
+                        tiles.setTileAt(tiles.getTileLocation(index2, index3), assets.tile`myTileperson`)
                     } else {
-                        tiles.setTileAt(tiles.getTileLocation(index2, index), assets.tile`obstacle-wall`)
+                        tiles.setTileAt(tiles.getTileLocation(index2, index3), assets.tile`obstacle-wall`)
                     }
                 }
             }
@@ -83,6 +128,7 @@ function PlaceObstacles (row: number, column: number) {
     }
 }
 let mySprite: Sprite = null
+let CurrentLevel = 0
 scene.setBackgroundImage(img`
     bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
     bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
@@ -208,7 +254,7 @@ scene.setBackgroundImage(img`
 tiles.setCurrentTilemap(tilemap`starfin`)
 mySprite = sprites.create(img`
     . . . . . . . . . . . . 
-    . . . . 2 2 2 . . . . . 
+    . . f f f f f f f . . . 
     . . . 2 2 2 2 2 . . . . 
     . c 4 4 2 2 2 4 4 c . . 
     . c f f d d d f f c . . 
@@ -218,9 +264,8 @@ tiles.placeOnTile(mySprite, tiles.getTileLocation(39, 113))
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
 let levels = [tileUtil.createSmallMap(tilemap`starfin`), 1]
-let CurrentLevel = 0
 PlaceObstacles(100, 100)
-mySprite.startEffect(effects.fountain)
+mySprite.startEffect(effects.none)
 if (controller.A.isPressed()) {
     mySprite.x = 33
     mySprite.y = 118
