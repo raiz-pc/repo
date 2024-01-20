c8cc515a1bf12c214741e8ccd1a7d298754a9c94
Thehut-Xbox <156123362+Thehut-Xbox@users.noreply.github.com>
2024-01-19 20:48:28 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts, Gokublack.ts, car.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,31 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    [img`
+        .......f..............f.......
+        .......fbbbbbfbfbbbbbbf.......
+        .......ffffffbdbfffffff.......
+        ..fffffffdddfbfbfddddfffffff..
+        .fbbbbbfffffbfffbffffffbbbbbf.
+        .fffffffddddbfffb8ddddfffffff.
+        .ffffffffbdd8fff86ddbffffffff.
+        .fffffffddddbfffb6ddddfffffff.
+        .fffffff6dddbbdbb6ddd6fffffff.
+        .fffffff8888fbdbf88888fffffff.
+        .fbbbbffffff886888ffffffbbbbf.
+        .fffffffffff88688ffffffffffff.
+        .fffffff8fff88688ffff8fffffff.
+        .fffffffffff88688ffffffffffff.
+        .fffffffbbbbb888bbbbbbfffffff.
+        ..ffff.f..............f.ffff..
+        `],
+    100,
+    false
+    )
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
@@ -23,6 +48,31 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         c c f f c c c c c f f c 
         . c c c c c c c c c c c 
         . . . . . . . . . . . . 
+        `],
+    100,
+    false
+    )
+})
+controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    [img`
+        .............fbf..............
+        .............bdb..............
+        ......ff..88fbdbf88..ff.......
+        .....fbbfbd8bbdbb8db8bbf......
+        .....fffdddddddddddddfff......
+        ..ffffffbbbbbbbbbbbbbffffff...
+        .fffffffffffffffffffffffffff..
+        .fbbbbbfffffffffffffffbbbbbf..
+        .fffffffff8fffefff8fffffffff..
+        .ffffffffffb8bdb8bffffffffff..
+        .fffffffffffffffffffffffffff..
+        .fffffffffffffffffffffffffff..
+        .ffffffffff8ffeff8ffffffffff..
+        .fffffffff88fffff88fffffffff..
+        .fffffffffff88888fffffffffff..
+        ..ffffff.fffff.ffffffffffff...
         `],
     100,
     false
@@ -43,50 +93,33 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    [img`
+        ..............................
+        ffff..........................
+        .bff........fff...............
+        .bbbff....ff..f.....fff.......
+        fffbbff.ff....f....fffff......
+        ffffbbff...eeef...fffffff.....
+        fffffbbffffffff...ffdddbff..f.
+        ffffffb888888888.ffbbbbbfffff.
+        fffbbf88ddddddddd6888888888ff.
+        fffbb888ddddddddd88888888888b.
+        ff888888dddddddd68fffffff888b.
+        fffffff88bbbbbb88fffffffffffb.
+        fffbffff8bbbbbb88ffffbffffdfb.
+        ffbbbffff88888888fffbbbfffdff.
+        fffbfffffffffffffffffbfffffff.
+        .fffff............fffffff.....
+        `],
+    100,
+    false
+    )
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTileperson`, function (sprite, location) {
-    for (let index = 0; index < 1; index++) {
-        animation.runImageAnimation(
-        mySprite,
-        [img`
-            . . . . . . . . . . . . 
-            . . . . . 4 4 . . . . . 
-            . . . . 4 5 5 4 . . . . 
-            . . . . 2 5 5 2 . . . . 
-            . . . . . 2 2 . . . . . 
-            . . . . . . . . . . . . 
-            `,img`
-            . . . . . . . . . . . . 
-            . . . . . . . . . . . . 
-            . . . . . . . . . . . . 
-            . . . . . . . . . . . . 
-            . . . . . . . . . . 4 . 
-            . . . . 2 . . . . 4 4 . 
-            `,img`
-            . 3 . . . . . . . . . . 
-            . 3 3 . . . . . . . . . 
-            . 3 d 3 . . 4 4 . . 4 4 
-            . . 3 5 3 4 5 5 4 4 d d 
-            . . 3 d 5 d 1 1 d 5 5 d 
-            . . 4 5 5 1 1 1 1 5 1 1 
-            `,img`
-            . . . . . . . . . . . . 
-            . . . . . . . . . . . . 
-            . . . . . b b . b b b . 
-            . . . . b 1 1 b 1 1 1 b 
-            . . b b 3 1 1 d d 1 d d 
-            . b 1 1 d d b b b b b 1 
-            `,img`
-            f f . 1 1 1 . . . . . . 
-            . 2 2 2 2 2 2 2 2 2 2 . 
-            c f f 2 2 2 2 2 f f 2 2 
-            c f f c c c c c f f c c 
-            c c c c c c c c c c c . 
-            . . . . . . . . . . . . 
-            `],
-        100,
-        false
-        )
-    }
+	
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -98,6 +131,31 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . c 2 2 2 2 2 2 2 c . . 
         . c f f 2 2 2 f f c . . 
         . c f f c c c f f c . . 
+        `],
+    100,
+    false
+    )
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    [img`
+        ..............................
+        ..........................ffff
+        ...............fff........ffb.
+        .......fff.....f..ff....ffbbb.
+        ......fffff....f....ff.ffbbfff
+        .....fffffff...feee...ffbbffff
+        .f..ffbdddff...ffffffffbbfffff
+        .fffffbbbbbff.888888888bffffff
+        .ff8888888886ddddddddd88fbbfff
+        .b88888888888ddddddddd888bbfff
+        .b888fffffff86dddddddd888888ff
+        .bfffffffffff88bbbbbb88fffffff
+        .bfdffffbffff88bbbbbb8ffffbfff
+        .ffdfffbbbfff88888888ffffbbbff
+        .fffffffbfffffffffffffffffbfff
+        .....fffffff............fffff.
         `],
     100,
     false
@@ -127,6 +185,52 @@ function PlaceObstacles (row: number, column: number) {
         }
     }
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    for (let index = 0; index < 1; index++) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . 
+            . . . . . 4 4 . . . . 1 
+            . . . . 4 5 5 4 . . . . 
+            . . . . 2 5 5 2 . . . . 
+            . . . . . 2 2 . . . . . 
+            . . . . . . . . . . . . 
+            `,img`
+            . . . 2 . . . 4 4 . . . 
+            . . . 2 4 . 4 5 4 . . . 
+            . . . . 2 4 5 5 4 . . . 
+            . . . . 2 5 5 5 4 . . . 
+            . . . . . 2 5 5 5 4 . . 
+            . . . . . 2 4 2 4 4 . . 
+            `,img`
+            . . . 4 5 4 4 4 d 4 . . 
+            . . . 3 d d d 5 5 4 . . 
+            . . 4 4 5 1 5 5 5 5 . . 
+            . . . 4 3 5 d 5 5 d . . 
+            . . 4 5 5 3 d 3 d 5 . . 
+            . . 4 4 d 4 d 4 3 d . . 
+            `,img`
+            . . b 3 1 d 1 d b . . . 
+            . b 1 1 . . . b d b . . 
+            . b 1 3 . . . . b 1 . . 
+            . b 1 b . . . b d d . . 
+            . b d d b d b d d b . . 
+            . . b b d d b b b . . . 
+            `,img`
+            . . . . . . . . . . . . 
+            . . f f f f f f f . . . 
+            . . . 2 2 2 2 2 . . . . 
+            . c 4 4 2 2 2 4 4 c . . 
+            . c f f 1 1 1 f f c . . 
+            . c f f c c c f f c . . 
+            `],
+        100,
+        false
+        )
+    }
+})
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 let CurrentLevel = 0
 scene.setBackgroundImage(img`
@@ -254,15 +358,41 @@ scene.setBackgroundImage(img`
 tiles.setCurrentTilemap(tilemap`starfin`)
 mySprite = sprites.create(img`
     . . . . . . . . . . . . 
-    . . f f f f f f f . . . 
-    . . . 2 2 2 2 2 . . . . 
-    . c 4 4 2 2 2 4 4 c . . 
-    . c f f d d d f f c . . 
-    . c f f c c c f f c . . 
+    . . . f f f f f f f . . 
+    . . . . 2 2 2 2 2 . . . 
+    . . c 4 4 2 2 2 4 4 c . 
+    . . c f f d d d f f c . 
+    . . c f f c c c f f c . 
     `, SpriteKind.Player)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(39, 113))
+scaling.scaleToPercent(mySprite, 270, ScaleDirection.Uniformly, ScaleAnchor.Middle)
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
+mySprite2 = sprites.create(img`
+    .............fbf..............
+    .............bdb..............
+    ......ff..88fbdbf88..ff.......
+    .....fbbfbd8bbdbb8db8bbf......
+    .....fffdddddddddddddfff......
+    ..ffffffbbbbbbbbbbbbbffffff...
+    .fffffffffffffffffffffffffff..
+    .fbbbbbfffffffffffffffbbbbbf..
+    .fffffffff8fffefff8fffffffff..
+    .ffffffffffb8bdb8bffffffffff..
+    .fffffffffffffffffffffffffff..
+    .fffffffffffffffffffffffffff..
+    .ffffffffff8ffeff8ffffffffff..
+    .fffffffff88fffff88fffffffff..
+    .fffffffffff88888fffffffffff..
+    ..ffffff.fffff.ffffffffffff...
+    `, SpriteKind.Player)
+controller.player2.moveSprite(mySprite2)
+tiles.placeOnTile(mySprite2, tiles.getTileLocation(33, 115))
+splitScreen.setSplitScreenEnabled(true)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.VerticalLeftHalf)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite2)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.VerticalRightHalf)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, mySprite)
 let levels = [tileUtil.createSmallMap(tilemap`starfin`), 1]
 PlaceObstacles(100, 100)
 mySprite.startEffect(effects.none)
@@ -270,3 +400,7 @@ if (controller.A.isPressed()) {
     mySprite.x = 33
     mySprite.y = 118
 }
+// work in progress enemy.
+game.onUpdateInterval(100000, function () {
+	
+})
